9fae1f8eb966a975200438754eff1aa5829471f2
Anirudh S <anirudh24seven@gmail.com>
2014-06-05 17:49:38 +0530

    Revert "Add authenticator helper which will use facebook id and secret"
    This reverts commit 748e064039cb1e1925c99b3229b5f7e20b637e6e.
    Reverting because I haven't figured out a proper way to authenticate facebook in a prod environment.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,9 +30,4 @@ module ApplicationHelper
       "#{page_title} | #{site_name}"
     end
   end
-
-  def authenticator
-    @authenticator ||= Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"], url("/auth/facebook/callback"))
-  end
-
 end
