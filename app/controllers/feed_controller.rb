7aee0e1b9d4c0c2753292e43a89cefbfafd0ff65
Anirudh S <anirudh24seven@gmail.com>
2014-06-09 10:04:48 +0530

    Show a list of categories to choose from when posting a link

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,17 +1,17 @@
 class FeedController < ApplicationController
   def development
-    @cards = Card.where("category LIKE ?", "development").reorder("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+    @cards = Card.where("LOWER(category) LIKE ?", "development").reorder("created_at DESC").paginate(:page => params[:page], :per_page => 10)
   end
 
   def design
-    @cards = Card.where("category LIKE ?", "design").reorder("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+    @cards = Card.where("LOWER(category) LIKE ?", "design").reorder("created_at DESC").paginate(:page => params[:page], :per_page => 10)
   end
 
   def business
-    @cards = Card.where("category LIKE ?", "business").reorder("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+    @cards = Card.where("LOWER(category) LIKE ?", "business").reorder("created_at DESC").paginate(:page => params[:page], :per_page => 10)
   end
 
   def growth
-    @cards = Card.where("category LIKE ?", "growth").reorder("created_at DESC").paginate(:page => params[:page], :per_page => 10)
+    @cards = Card.where("LOWER(category) LIKE ?", "growth").reorder("created_at DESC").paginate(:page => params[:page], :per_page => 10)
   end
 end
